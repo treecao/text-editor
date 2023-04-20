bffc6a25127ee76ac296d2cbf5fe7b18a1c24c82
treecoww <tcao89@gmail.com>
2023-04-20 09:26:55 -0500

    add entries for module exports

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,15 +3,17 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add/configure workbox plugins for a service worker and manifest file.Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
